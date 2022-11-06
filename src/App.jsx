4f182b526ebe3a73f8f5990e75c9677b8b52fd56
Robert Butler <robertbutler@protonmail.com>
2022-11-06 01:04:16 +0000

    feat: add nav and link to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import Nav from './components/Nav';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/:pokemonName' element={<Detail />} />
-    </Routes>
+    <main id='app-container'>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:pokemonName' element={<Detail />} />
+      </Routes>
+    </main>
   );
 }
